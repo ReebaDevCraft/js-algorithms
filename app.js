a0ffe060c402cfe3dff95b7632e54b9843d7cf02
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2024-12-31 22:34:20 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("I am JS ");
+fullName = 'Reeba Yaseen';
+age = '45';
+console.log(age);
+isFollow = (true);
+console.log(isFollow);
+let Num = '44';
+console.log(Num);
+const PI= '3.14';
+console.log(PI);
+const student = {
+     name: 'Ali',
+     age:'23',
+     cgpa: '2.70',
+     isPass: true
+}
+const Product={
+    offer: 'Pen touch',
+    deal:'30%',
+    price:'$44',
+    rating:'4.5'
+}
+console.log(Product);
+
+const Profile={
+    tittle:'Instagram',
+    post: '144',
+    isFollow:true
+};
+console.log(Profile);
