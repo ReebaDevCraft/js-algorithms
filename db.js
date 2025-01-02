f0bf2b939cab33e3e4b236a8e0f687867b00944f
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-01-02 23:39:22 +0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,26 @@
+export const user = [
+  {
+    name: "Jhon",
+    id: "1",
+    age: 23,
+    email: "johndoe@example.com",
+  },
+  {
+    name: "Hary",
+    id: "2",
+    age: 25,
+    email: "hary@example.com",
+  },
+  {
+    name: "Petter",
+    id: "30",
+    age: 3,
+    email: "pettere@example.com",
+  },
+  {
+    name: "Fari",
+    id: "4",
+    age: 5,
+    email: "farie@example.com",
+  },
+];
