544af93cef30afecc0277abd0b7037058acf9eee
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-01-07 21:03:37 +0500

    Update algo.js
    JS update

--- algo.js
+++ algo.js
@@ -2,3 +2,8 @@ function myfunction(){
     console.log('Hi we are JS algorithm')
 }
 myfunction();
+
+function classfunction(msg){
+    console.log(msg)
+}
+classfunction('JS is very easy to learn');
