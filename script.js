df059844ae196a29531b57172c8b37571da8184f
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-02-26 23:55:49 +0500

    Update script.js
    fetch API and convert into json

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Wait for the document to fully load before executing scripts
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Page Loaded!");
-
-    // Example: Change text content on button click
     document.getElementById("changeTextBtn").addEventListener("click", function () {
         document.getElementById("message").textContent = "Text Updated!";
     });
 
-    // Example: Fetch data from an API
     fetch("https://jsonplaceholder.typicode.com/posts/1")
         .then(response => response.json())
         .then(data => console.log("Fetched Data:", data))
