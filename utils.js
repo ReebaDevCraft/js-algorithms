fc13988c327d72b9bc4bdaa8f7b885ea795ea260
Reeba <167805857+ReebaDevCraft@users.noreply.github.com>
2025-02-26 23:46:19 +0500

    Update utils.js
    To improve maintainability by separating logic from the main script.

--- utils.js
+++ utils.js
@@ -1,1 +1,21 @@
+// Check if a number is even or odd
+function checkEvenOdd(num) {
+    return num % 2 === 0 ? "Even" : "Odd";
+}
+
+// Capitalize the first letter of a string
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+// Generate a random number between a given range
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Format the current date as YYYY-MM-DD
+function getFormattedDate() {
+    let date = new Date();
+    return date.toISOString().split('T')[0];
+}
 
